cba2ae56934476f8be5f83cead3349b246114f09
dolchain <dolchain0@gmail.com>
2023-10-12 18:41:23 -0400

    add the PORT varaible to the server

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-const ethers = require('ethers');
-const {Transfered} = require('./supabase')
-// Setup Contract
-const { abi } = require('./abis/usdcTestToken.json')
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const tokenListen = require('./tokenListen');
 
-const usdcAddress = "0xc493e7373757C759cf589731eE1cFaB80b13Ed7a";
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-const provider = new ethers.JsonRpcProvider('https://eth-sepolia.g.alchemy.com/v2/demo');
-const usdcToken = new ethers.Contract(usdcAddress, abi, provider);
-
-try {
-  usdcToken.on('Transfer', (from, to, value, event) => {
-    // console.log('Event Fired:', event);
-    console.log('Parameters:', from, to, value);
-    // updateUserBalance(from, to, -parseFloat(ethers.formatUnits(value, 6)));
-    Transfered(event.log.transactionHash, from, to, parseFloat(ethers.formatUnits(value, 6)));
-  });
-} catch (error) {
-  console.error("Error setting up event listener:", error);
-}
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  tokenListen();
+});
